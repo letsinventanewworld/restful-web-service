8261778c2b683cf1a86b9fd72ed08ccc3aa0d1fa
elakioui <zouhaire.elakioui@gmail.com>
2019-03-21 10:46:04 +0000

    adding path variable

---
+++ src/main/java/com/tonasolution/rest/webservices/restfulwebservices/entity/HelloWorldBean.java
@@ -0,0 +1,31 @@
+package com.tonasolution.rest.webservices.restfulwebservices.entity;
+
+public class HelloWorldBean {
+	
+	private String message;
+
+	public HelloWorldBean() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public HelloWorldBean(String message) {
+		super();
+		this.message = message;
+	}
+
+
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+}
