8bef89fce55525d0180988f238d2987d55bea38a
elakioui <zouhaire.elakioui@gmail.com>
2019-03-25 10:14:59 +0000

    create a new user - return link

--- src/main/java/com/tonasolution/rest/webservices/restfulwebservices/controllers/UserController.java
+++ src/main/java/com/tonasolution/rest/webservices/restfulwebservices/controllers/UserController.java
@@ -1,13 +1,16 @@
 package com.tonasolution.rest.webservices.restfulwebservices.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tonasolution.rest.webservices.restfulwebservices.dao.UserDaoService;
 import com.tonasolution.rest.webservices.restfulwebservices.entity.User;
@@ -29,11 +32,14 @@ public class UserController {
 	}
 	
 	@PostMapping("/users")
-	public User createUser(@RequestBody User user) {
-		
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User savedUser = userDaoService.save(user);
-		return savedUser;
-	
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+								.buildAndExpand(savedUser.getId())
+								.toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 	
 	
