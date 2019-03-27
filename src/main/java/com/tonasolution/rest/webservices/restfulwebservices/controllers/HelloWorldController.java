8a0c6517c14509f2f639199186be776648a2e529
elakioui <zouhaire.elakioui@gmail.com>
2019-03-27 12:49:06 +0000

    internationalization simplified config

--- src/main/java/com/tonasolution/rest/webservices/restfulwebservices/controllers/HelloWorldController.java
+++ src/main/java/com/tonasolution/rest/webservices/restfulwebservices/controllers/HelloWorldController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -25,8 +26,8 @@ public class HelloWorldController {
 	}
 	
 	@GetMapping(path = "/hello-world-internationalized")
-	public String helloWorlderInternationalized(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
-		return messageSource.getMessage("good.morning.message", null, locale);
+	public String helloWorlderInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
