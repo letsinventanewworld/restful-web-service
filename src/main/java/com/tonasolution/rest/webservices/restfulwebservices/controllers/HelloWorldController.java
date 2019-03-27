6c9cdce69a2d543b0c7d0a15a817a5dfeaf0faea
elakioui <zouhaire.elakioui@gmail.com>
2019-03-27 12:37:16 +0000

    add internationalization

--- src/main/java/com/tonasolution/rest/webservices/restfulwebservices/controllers/HelloWorldController.java
+++ src/main/java/com/tonasolution/rest/webservices/restfulwebservices/controllers/HelloWorldController.java
@@ -1,7 +1,12 @@
 package com.tonasolution.rest.webservices.restfulwebservices.controllers;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +16,17 @@ import com.tonasolution.rest.webservices.restfulwebservices.entity.HelloWorldBea
 @RestController
 public class HelloWorldController {
 	
+	@Autowired
+	private MessageSource messageSource;
+	
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorlderInternationalized(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
 	}
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
