5ad2132c53ccfdd158c42cbe057e615613364d3c
elakioui <zouhaire.elakioui@gmail.com>
2019-03-29 11:40:38 +0000

    enhanced api doc

--- src/main/java/com/tonasolution/rest/webservices/restfulwebservices/config/SwaggerConfig.java
+++ src/main/java/com/tonasolution/rest/webservices/restfulwebservices/config/SwaggerConfig.java
@@ -1,7 +1,14 @@
 package com.tonasolution.rest.webservices.restfulwebservices.config;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,7 +17,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 	
+	public static final Contact DEFAULT_CONTACT = new Contact("EL AKIOUI Zouhaire", "tonasolution.com", "contact@tonasolution.com");
+	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Good Documentation", "Good Api Documentation", "1.0", "urn:tos",
+	          DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+	private static final Set<String> DEFAULT_PRODUCES_CONSUMES = new HashSet(Arrays.asList("application/json", "application/xml"));
+
+	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2)
+					.apiInfo(DEFAULT_API_INFO)
+					.produces(DEFAULT_PRODUCES_CONSUMES)
+					.consumes(DEFAULT_PRODUCES_CONSUMES);
 	}
 }
