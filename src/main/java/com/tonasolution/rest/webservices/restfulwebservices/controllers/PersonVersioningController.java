4134927188b7275ea715b9f5687496051ee8ea2b
elakioui <zouhaire.elakioui@gmail.com>
2019-04-01 11:52:12 +0000

    versioning approch 2/2

--- src/main/java/com/tonasolution/rest/webservices/restfulwebservices/controllers/PersonVersioningController.java
+++ src/main/java/com/tonasolution/rest/webservices/restfulwebservices/controllers/PersonVersioningController.java
@@ -10,12 +10,30 @@ import com.tonasolution.rest.webservices.restfulwebservices.entity.PersonV2;
 @RestController
 public class PersonVersioningController {
 	
-	@GetMapping("v2/person")
+	@GetMapping(value="/person", params="version=2")
 	public PersonV2 personV2() {
 		return new PersonV2("Bob charlie");
 	}
-	@GetMapping("v1/person")
+	@GetMapping(value="/person", params="version=1")
 	public PersonV1 personV1() {
+		return new PersonV1(new Name("Bob", "Charlie"));
+	}
+	
+	@GetMapping(value="/person/header", headers="X-API-VERSION=2")
+	public PersonV2 personXAPIVERSIONV2() {
+		return new PersonV2("Bob charlie");
+	}
+	@GetMapping(value="/person/header", headers="X-API-VERSION=1")
+	public PersonV1 personXAPIVERSIONV1() {
+		return new PersonV1(new Name("Bob", "Charlie"));
+	}
+	
+	@GetMapping(value="/person/produces", produces="application/tonasolution.company.app-v2+json")
+	public PersonV2 personproducesv2() {
+		return new PersonV2("Bob charlie");
+	}
+	@GetMapping(value="/person/produces", produces="application/tonasolution.company.app-v1+json")
+	public PersonV1 personproducesv1() {
 		return new PersonV1(new Name("Bob", "Charlie"));
 	}
 }
