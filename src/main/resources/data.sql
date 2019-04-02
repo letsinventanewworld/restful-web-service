4bdbb2888ca0cef2f678670908b8fdd36e8c7736
elakioui <zouhaire.elakioui@gmail.com>
2019-04-02 09:40:24 +0000

    make user class as en antity

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user values (1, sysdate(), 'ABgg');
+insert into user values (2, sysdate(), 'Bill');
+insert into user values (3, sysdate(), 'Ali');
+insert into user values (4, sysdate(), 'Money');
